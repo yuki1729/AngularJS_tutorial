2e574c7e465692e22b891f1f286fba44eeff065c
yuki1729 <yuki.o1729@gmail.com>
2015-09-08 23:43:01 +0900

    コード自動整形

--- todo.js
+++ todo.js
@@ -1,12 +1,19 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
-    todoList.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+    todoList.todos = [{
+      text: 'learn angular',
+      done: true
+    }, {
+      text: 'build an angular app',
+      done: false
+    }];
 
     todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todos.push({
+        text: todoList.todoText,
+        done: false
+      });
       todoList.todoText = '';
     };
 
